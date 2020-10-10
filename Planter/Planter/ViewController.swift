8db323ec2aace63779f6c47f6f122b2b261b9ae6
MichaelPassiflora <68521262+MichaelPassiflora@users.noreply.github.com>
2020-10-09 22:44:01 -0500

    content

--- Planter/Planter/ViewController.swift
+++ Planter/Planter/ViewController.swift
@@ -14,6 +14,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func plant(_ sender: Any) {
+    }
+    @IBAction func ID(_ sender: Any) {
+    }
+    
+    @IBAction func Calender(_ sender: Any) {
+    }
+    @IBAction func Profile(_ sender: Any) {
+    }
 }
 
+{let btnName = UIButton()
+btnName.setImage(UIImage(named: "ID"), forState: .Normal)
+{btnName.frame = CGRectMake(0, 0, 30, 30)
+}{btnName.addTarget(self, action: Selector("action"), forControlEvents: .TouchUpInside)
+
+
+
+}{let rightBarButton = UIBarButtonItem()
+rightBarButton.customView = btnName
+
+
+}{self.navigationItem.rightBarButtonItem = rightBarButton}
